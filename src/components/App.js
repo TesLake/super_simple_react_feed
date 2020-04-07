e075507c69b4861b9ad6b60fbf2edf39a30a360a
Jorge <user@example.com>
2020-04-07 15:15:33 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import PostsList from './PostsList'
+
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <PostsList />
+        </div>
+    )
+}
+
+export default App
